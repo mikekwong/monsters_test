3d7fb1d73f688bac48daf1c830f3d8596a93f349
Mike Wong <wongmdesign@gmail.com>
2020-02-01 22:40:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import CardList from './components/CardList/CardList'
+import './App.css'
 
-function App() {
+function App () {
+  const [data, setData] = useState({ monsters: [] })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setData(data)
+    }
+    fetchData()
+  }, [])
+
+  console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CardList name='Yihua'>
+        {data.length &&
+          data.map(monster => {
+            return <p key={monster.id}>{monster.name}</p>
+          })}
+      </CardList>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CardList.css'
+
+const CardList = ({ children }) => {
+  return <div className='card-list'>{children}</div>
+}
+
+export default CardList
